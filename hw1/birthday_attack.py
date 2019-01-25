a5107e68e5fd5d971da45360af406ddd1a008cb1
Branden Kim <brandenk0@gmail.com>
2019-01-24 21:11:50 -0500

    Complete birthday_attack

--- hw1/birthday_attack.py
+++ hw1/birthday_attack.py
@@ -2,36 +2,27 @@ import hashlib
 import random
 
 
-def generate_different_ascii_str(str_len=50):
-    first = ''.join([chr(random.randint(0, 255)) for _ in range(str_len)])
-    second = ''.join([chr(random.randint(0, 255)) for _ in range(str_len)])
-    
-    while first == second:
-        first, second = random.randint(0, 255), random.randint(0, 255)
-    
-    return first, second
-
-
-
-def equal_sha(first, second):
-    first_hash = hashlib.sha1(first.encode('utf-8')).hexdigest()
-    second_hash = hashlib.sha1(second.encode('utf-8')).hexdigest()
-
-    return first_hash[:10] == second_hash[:10]
+def generate_ascii_str(str_len=50, num=1000000):
+    current = 0
+    while current < num:
+        ascii_str = ''.join([chr(random.randint(0, 255)) for _ in range(str_len)])
+        yield hashlib.sha1(ascii_str.encode('utf-8')).hexdigest()[:10], ascii_str
+        current += 1
 
 
 def find_sha_pair():
-    first, second = generate_different_ascii_str()
-    num_attempts = 1
+    num_attempts, found, hashes = 0, False, {}
 
-    while not equal_sha(first, second):
-        first, second = generate_different_ascii_str()
-        num_attempts += 1
-        if num_attempts % 100000 == 0:
-            print(f'Hit another multiple of 100,000: {num_attempts}')
+    while True:
+        for hash_str, ascii_str in generate_ascii_str():
+            num_attempts += 1
+            if hash_str in hashes:
+                return ascii_str, hashes[hash_str], num_attempts
+            hashes[hash_str] = ascii_str
+        else:
+            print('Went through a million ascii strings... Trying again')
 
-    return first, second, num_attempts
 
 
 if __name__ == '__main__':
-    print('Found (%s, %s) pair in %i attempts' % find_sha_pair())
+    print('First:\n%s\nSecond:\n%s\nPair in %i attempts' % find_sha_pair())
