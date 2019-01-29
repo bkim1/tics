dfaee61479a1cd9851237b38fa9321ccbdc20e20
Branden Kim <brandenk0@gmail.com>
2019-01-29 11:19:24 -0500

    Refactor a few things for HW1

--- hw1/birthday_attack.py
+++ hw1/birthday_attack.py
@@ -2,7 +2,7 @@ import hashlib
 import random
 
 
-def generate_ascii_str(str_len=50, num=1000000):
+def generate_ascii_sha_pair(str_len=30, num=1000000):
     current = 0
     while current < num:
         ascii_str = ''.join([chr(random.randint(0, 255)) for _ in range(str_len)])
@@ -14,10 +14,10 @@ def find_sha_pair():
     num_attempts, found, hashes = 0, False, {}
 
     while True:
-        for hash_str, ascii_str in generate_ascii_str():
+        for hash_str, ascii_str in generate_ascii_sha_pair():
             num_attempts += 1
             if hash_str in hashes:
-                return ascii_str, hashes[hash_str], num_attempts
+                return ascii_str, hashes[hash_str], hash_str, num_attempts
             hashes[hash_str] = ascii_str
         else:
             print('Went through a million ascii strings... Trying again')
@@ -25,4 +25,6 @@ def find_sha_pair():
 
 
 if __name__ == '__main__':
-    print('First:\n%s\nSecond:\n%s\nPair in %i attempts' % find_sha_pair())
+    first, second, hash_str, num_attempts = find_sha_pair()
+    print(f'({first}, {second}, {num_attempts})')
+    print(f'Hash: {hash_str}')  
