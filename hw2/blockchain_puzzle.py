14dd00113a726db43cd4e411d169fec32abbb2b9
Branden Kim <brandenk0@gmail.com>
2019-02-07 22:42:26 -0500

    Update to working code

--- hw2/blockchain_puzzle.py
+++ hw2/blockchain_puzzle.py
@@ -3,41 +3,23 @@ import os
 import uuid
 import hashlib
 
-import matplotlib
-
-
-class BitcoinHeader():
-    
-    def __init__(self):
-        self.hash_val = None
-        self.timestamp = time.time()
-        self.prev = None
-        self.root = None
-        self.nonce = self.generate_nonce()
-        self.target = None
-    
-    def generate_nonce(self, length=8):
-        return uuid.uuid4().hex[:length]
-    
-    def set_new_nonce(self):
-        self.nonce = self.generate_nonce()
-
-
+import matplotlib.pyplot as plt
 
 
 def test_difficulty(difficulty, prev_hash, new_hash, nonce, start_time):
     m = hashlib.sha256()
-    temp = hash(nonce + prev_hash + new_hash)
-    num_attempts = 1
+    temp = int(hashlib.sha256(str(nonce + prev_hash + new_hash).encode('utf-8')).hexdigest(), 16)
+    num_attempts, target = 1, 2**difficulty
 
-    while temp > difficulty:
+    while temp > target:
         nonce += 1
-        temp = hash(nonce + prev_hash + new_hash)
+        # temp = hash(nonce + prev_hash + new_hash)
+        temp = int(hashlib.sha256(str(nonce + prev_hash + new_hash).encode('utf-8')).hexdigest(), 16)
 
-        if num_attempts % 50000000 == 0:
+        if num_attempts % 10000000 == 0:
             if time.time() - start_time > 300:
                 print('5-minutes up!')
-                break
+                return -1, -1
             print(f'{num_attempts} have gone by')
         
         num_attempts += 1
@@ -52,22 +34,42 @@ def get_time(difficulty):
     print(f'Prev: {prev_hash}, Nonce: {nonce}')
     start = time.time()
     end, num_attempts = test_difficulty(difficulty, prev_hash, new_hash, nonce, start)
+
+    if end == -1 and num_attempts == -1:
+        return None
     print(end - start)
 
-    return end - start, num_attempts
+    return end - start
 
 
 
 def generate_data(difficulty, num_data_points=5):
-    vector = [get_time(difficulty) for _ in range(num_data_points)]
+    vector, num_attempts = [], 1
+    while len(vector) < 5:
+        if num_attempts == 3:
+            return None
 
-    print(vector)
+        result = get_time(difficulty)
+
+        if result is not None:
+            vector.append(result)
+        else:
+            num_attempts += 1
+
+    return vector
 
 
-def main():
-    difficulty = 2**35
-    generate_data(difficulty)
+def generate_plots(difficulty=235):
+    i, results, temp = 0, [], []
+    while temp is not None:
+        temp = generate_data(difficulty - i)
+        i += 1
+        results.append(temp)
+        
 
+    fig_1, ax_1 = plt.subplots()
+    ax_1.boxplot(results, vert=True)
+    plt.show()
 
 if __name__ == '__main__':
-    main()
+    generate_plots()
