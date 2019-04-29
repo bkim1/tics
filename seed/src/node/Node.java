e0e9c2751af3dc2fc84e8f76eddc06d4e73e4226
Branden Kim <brandenk0@gmail.com>
2019-04-28 20:55:32 -0400

    Make Node object synchronized

--- seed/src/node/Node.java
+++ seed/src/node/Node.java
@@ -19,6 +19,8 @@ public class Node {
     private Map<String, PeerData> peerFiles;
     private Peer predecessor;
     private Map<String, FileInfo> myFiles;
+    
+    private static Object sharedLock = new Object();
 
     public Node(InetAddress ip, int port) {
         this.ip = ip;
@@ -28,50 +30,120 @@ public class Node {
         this.myFiles = new HashMap<>();
     }
 
-    public String getKey() { return this.password; }
+    public String getKey() {
+        synchronized(sharedLock) {
+            return this.password;
+        }
+    }
     private void generateKey() {}
 
-    public long getPeerId() { return this.peerId; }
-
-    public synchronized void updateAddress(InetAddress ip, int port) {
-        this.ip = ip;
-        this.port = port;
-        this.peerId = generatePeerId(this.ip, this.port);
+    public long getPeerId() {
+        synchronized(sharedLock) {
+            return this.peerId;
+        }
     }
 
-    public InetAddress getIP() { return this.ip; }
-    public synchronized void setIP(InetAddress ip) {
-        this.ip = ip;
-        this.peerId = generatePeerId(this.ip, this.port);
+    public void updateAddress(InetAddress ip, int port) {
+        synchronized(sharedLock) {
+            this.ip = ip;
+            this.port = port;
+            this.peerId = generatePeerId(this.ip, this.port);
+        }
     }
 
-    public int getPort() { return this.port; }
-    public synchronized void setPort(int port) {
-        this.port = port;
-        this.peerId = generatePeerId(this.ip, this.port);
+    public InetAddress getIP() {
+        synchronized(sharedLock) {
+            return this.ip;
+        }
+    }
+    public void setIP(InetAddress ip) {
+        synchronized(sharedLock) {
+            this.ip = ip;
+            this.peerId = generatePeerId(this.ip, this.port);
+        }
     }
 
-    public Peer getPeerObject() { return new Peer(this.ip, this.port, this.peerId); }
+    public int getPort() {
+        synchronized(sharedLock) {
+            return this.port;
+        }
+    }
+    public void setPort(int port) {
+        synchronized(sharedLock) {
+            this.port = port;
+            this.peerId = generatePeerId(this.ip, this.port);
+        }
+    }
 
-    public Peer getSuccessor() { return this.fingerTable[0]; }
-    public synchronized void setSuccessor(Peer p) { this.fingerTable[0] = p; }
+    public Peer getPeerObject() {
+        synchronized(sharedLock) {
+            return new Peer(this.ip, this.port, this.peerId);
+        }
+    }
 
-    public Peer getPredecessor() { return this.predecessor; }
-    public synchronized void setPredecessor(Peer p) { this.predecessor = p; }
+    public Peer getSuccessor() {
+        synchronized(sharedLock) {
+            return this.fingerTable[0];
+        }
+    }
+    public void setSuccessor(Peer p) {
+        synchronized(sharedLock) {
+            this.fingerTable[0] = p;
+        }
+    }
 
-    public Peer[] getFingerTable() { return this.fingerTable; }
-    public synchronized void updateFingerTable(Peer[] fingerTable) { this.fingerTable = fingerTable; }
-    public synchronized void updateFingerTable(Peer peer, int index) { this.fingerTable[index] = peer; }
+    public Peer getPredecessor() {
+        synchronized(sharedLock) {
+            return this.predecessor;
+        }
+    }
+    public void setPredecessor(Peer p) {
+        synchronized(sharedLock) {
+            this.predecessor = p; 
+        }
+    }
 
-    public Map<String, PeerData> getPeerFiles() { return this.peerFiles; }
+    public Peer[] getFingerTable() {
+        synchronized(sharedLock) {
+            return this.fingerTable;
+        }
+    }
+    public void updateFingerTable(Peer[] fingerTable) {
+        synchronized(sharedLock){
+            this.fingerTable = fingerTable; 
+        }
+    }
+    public void updateFingerTable(Peer peer, int index) {
+        synchronized(sharedLock) {
+            this.fingerTable[index] = peer;
+        }
+    }
+
+    public Map<String, PeerData> getPeerFiles() {
+        synchronized(sharedLock) {
+            return this.peerFiles;
+        }
+    }
     public PeerData getPeerData(long key) {
-        return this.peerFiles.get(Long.toString(key));
+        synchronized(sharedLock) {
+            return this.peerFiles.get(Long.toString(key));
+        }
     }
-    public synchronized void addPeerFile(PeerData data) {
+    public void addPeerFile(PeerData data) {
         String strKey = Long.toString(data.getKey());
-        this.peerFiles.put(strKey, data);
+        synchronized(sharedLock) {
+            this.peerFiles.put(strKey, data);
+        }
     }
 
-    public Map<String, FileInfo> getMyFiles() { return this.myFiles; }
-    public synchronized void addFile(FileInfo file) { this.myFiles.put(file.getFilename(), file); }
+    public Map<String, FileInfo> getMyFiles() {
+        synchronized(sharedLock) {
+            return this.myFiles;
+        }
+    }
+    public void addFile(FileInfo file) {
+        synchronized(sharedLock) {
+            this.myFiles.put(file.getFilename(), file);
+        }
+    }
 }
