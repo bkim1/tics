f15b4548210b17074ca8654c7805e8cc69e046f0
Branden Kim <brandenk0@gmail.com>
2019-05-01 12:23:21 -0400

    Refactor rcving a file to grab data from msg

--- seed/src/thread/ReceiveFileThread.java
+++ seed/src/thread/ReceiveFileThread.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import node.*;
@@ -16,20 +17,30 @@ public class ReceiveFileThread implements Runnable {
     private NodeController nc;
     private Socket socket;
     private InputStream inputStream;
+    private OutputStream outputStream;
+    private String downloadLoc;
+    private FileInfo info;
 
-    public ReceiveFileThread(Message msg, NodeController nc, Socket peerSocket) throws IOException {
+    public ReceiveFileThread(Message msg, NodeController nc, Socket peerSocket) {
         this.msg = msg;
         this.nc = nc;
         this.socket = peerSocket;
-        this.inputStream = this.socket.getInputStream();
+        
+        try {
+            this.inputStream = this.socket.getInputStream();
+            this.outputStream = this.socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.downloadLoc = this.nc.getDownloadLoc();
     }
     
     public void run() {
         // Use Socket (not ServerSocket) to open connection using 
         // Peer's info from msg
         try {
-            String filename = this.getFile();
-            if (this.verify(filename)) {
+            FileInfo info = this.getFile();
+            if (this.verify(info)) {
 
             }
         } catch(IOException e) {
@@ -37,29 +48,32 @@ public class ReceiveFileThread implements Runnable {
         }
     }
 
-    private String getFile() throws IOException {
-        String downloadLoc = this.nc.getDownloadLoc();
-        String fileName = downloadLoc + this.nc.getFilename(this.msg.getKey());
-        File outputFile = new File(fileName);
+    private FileInfo getFile() throws IOException {
+        String absFileName = this.downloadLoc + this.nc.getFilename(this.msg.getKey());
+        File outputFile = new File(absFileName);
         outputFile.createNewFile();
+        FileOutputStream writer = new FileOutputStream(outputFile);
+        
+        byte[] data = this.msg.getData();
+        writer.write(data);
 
-        int dataSize;
-        byte[] buffer = new byte[4096];
-        BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(outputFile));
-
-        while ((dataSize = this.inputStream.read(buffer)) > 0) {
-            writer.write(buffer, 0, dataSize);
-        }
-
-        System.out.println("Received the file!");
+        System.out.println("Downloaded  the file!");
         writer.flush();
         writer.close();
+        
+        // int numRetry = 0;
+        FileInfo info = this.nc.getLookupFileInfo(this.msg.getKey());
+        // while (info == null && numRetry > 3) {
+        //     Message retryMsg = new Message(ReqType.SEND_FAIL, this.nc.getPeerObject());
+        // }
         this.socket.close();
-
-        return fileName;
+        return info;
     }
 
-    private boolean verify(String filename) {
+    private boolean verify(FileInfo info) {
+        byte[] salt = info.getSalt();
+
+
         return false;
     }
 }
