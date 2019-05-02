1062f3ebebf32d015d79beee5c992682d8451888
Branden Kim <brandenk0@gmail.com>
2019-05-02 16:33:53 -0400

    comment some print statements

--- seed/src/utils/Utilities.java
+++ seed/src/utils/Utilities.java
@@ -33,8 +33,8 @@ public class Utilities {
 			int nodeKey = finger.getKey();
 			int targetKey = msg.getKey();
 
-			System.out.println("Node Key: " + Integer.toString(nodeKey));
-			System.out.println("Target Key: " + Integer.toString(targetKey));
+			// System.out.println("Node Key: " + Integer.toString(nodeKey));
+			// System.out.println("Target Key: " + Integer.toString(targetKey));
 			/*
 			if(finger.equals(msg.getPeer())) {
 				return null;
